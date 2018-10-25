0b36f28d38b6d5f1029d77f319cc032ee15037ec
jasper.he <jasper.he@shein.com>
2018-10-25 17:57:22 +0800

    1

---
+++ sizeof.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    float f,x=3.6,y=5.2;
+    int i=4,a,b;
+    a=x+y;
+    b=(int)(x+y);
+    float chu = 10;
+    f=chu/i;
+    //printf(f);
+   	printf("a=%d,b=%d,f=%f,x=%f\n",a,b,f,x);
+}
